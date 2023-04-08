8ca5a96bb631fa134fe8089bfe96dcb017ae67a8
AdityaSurve <surveaditya521@gmail.com>
2023-04-09 04:23:26 +0530

    profile

--- client/src/Components/Profile.js
+++ client/src/Components/Profile.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Navbar } from "./Home/Navbar";
-import { Wallet } from "./Profile/Wallet";
+import { ProfileCard } from "./Profile/Profilecard";
 
 function Profile() {
   return (
@@ -8,9 +8,22 @@ function Profile() {
       <div>
         <Navbar />
       </div>
-      <div className="mt-6 ms-3">
-        <h1 className="text-3xl font-thin">Username</h1>
-        <Wallet/>
+      <div className="w-[50vw] mt-10 flex flex-col gap-3 justify-center text-xl text-center mx-auto rounded-md bg-[#DEAD84] p-5">
+        <div className="flex flex-row justify-between">
+          <h1 className="font-thin">Username</h1>
+          <h1 className="font-thin">Email</h1>
+        </div>
+        <hr className="border-[#563300]" />
+        <h1 className="font-thin">Points</h1>
+      </div>
+      <h3 className="mt-10 text-[#563300] flex justify-center text-center mx-auto">
+        Your Coupons
+      </h3>
+      <div className="mt-10 flex flex-col gap-2 justify-center items-center text-center mx-auto">
+        <ProfileCard />
+        <ProfileCard />
+        <ProfileCard />
+        <ProfileCard />
       </div>
     </div>
   );
